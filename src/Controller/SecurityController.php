55f763642885ca4eaf76cd9b8fd97d23ff0dfeac
Damien FASSIAUX <damienfassiaux@gmail.com>
2021-03-23 11:52:38 +0100

    sécurité form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,17 +7,31 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Utilisateurs;
 use App\Form\InscriptionType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/inscription", name="security_inscription")
      */
-    public function formInscription(): Response
+    public function formInscription(Request $request, EntityManagerInterface $manager): Response
     {   
         $utilisateur = new Utilisateurs;
 
         $form = $this->createForm(InscriptionType::class, $utilisateur);
+
+        $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid())
+            {
+                $manager->persist($utilisateur); 
+                $manager->flush(); 
+
+                $this->addFlash('success', "félicitations !! Votre compte a bien été validé ! vous pouvez dès à présent vous connecter");
+                return $this->redirectToRoute('site');
+            }
+
 
         return $this->render('security/inscription.html.twig', [
            'formInscription' => $form ->createView()
